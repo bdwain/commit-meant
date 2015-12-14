a713bc6cfde81e5e50e347b53517112860bd5555
Isaiah Simpson <isaiah.simpson@gmail.com>
2015-12-14 10:42:57 -0600

    remove nodegit dependency in favor of simpler git CLI implementation. simplify options.

--- index.js
+++ index.js
@@ -4,27 +4,27 @@
 
 'use strict';
 
-let _ = require('lodash'),
-    path = require('path'),
-    Git = require('nodegit'),
-    program = require('commander');
-
-
-const NO_COMMIT_MEANT = 'No commit-meant found.',
-    messageRe = /^(MAJOR|MINOR|PATCH) - (.+)\n\n(.+)\n\n((?:[^\*+-].+(?:\n\n)*)*)((?:[\*]\s.+\n*)*)/g,
-    noteRe = /[\*+-]\s/g;
+const
+    _ = require('lodash'),
+    exec = require('child_process').exec,
+    program = require('commander'),
+    NO_COMMIT_MEANT = 'No commit-meant found.',
+    messageRe = /^(MAJOR|MINOR|PATCH) - ([A-Z].+)\n\n((?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?)\n\n((?:[^\*+-].+(?:\n\n)*)*)((?:[\*]\s.+\n*)*)/g,
+    noteRe = /[\*+-]\s/g,
+    LOG_SEPARATOR = 'GJVX47gWz4@7m&*uYX%5qe24';
 
 function message2cm(msg) {
     let json = msg
         .replace(messageRe, '{ "changeType": "$1", "title": "$2", "issue": "$3", "description": "$4", "notes": "$5" }')
         .replace(/\n/g, ''),
-        obj;
+        cm;
 
     try {
-        obj = JSON.parse(json);
-        obj.notes = _.filter(_.map(obj.notes.trim().split(noteRe), note => note.trim()), note => note);
+        cm = JSON.parse(json);
+        cm.changeType = cm.changeType.toLowerCase();
+        cm.notes = _.filter(_.map(cm.notes.trim().split(noteRe), note => note.trim()), note => note);
 
-        return obj;
+        return cm;
     } catch (e) {
         return null;
     }
@@ -34,17 +34,8 @@ program
     .version('0.1.0')
     .usage('[options] [branchName]')
     .option('-t, --tip', 'only check the tip of the branch for a commit-meant')
-    .option('-a, --all', 'check all commits on the branch for a commit-meant')
-    .option('-l, --log <n>', 'output commit-meant history for last n commits on the branch')
-
     .option('-f, --field <name>', 'output the value of the commit-meant field with the specified name')
-
     .parse(process.argv);
-
-let pwd = path.resolve('.'),
-    repo,
-    masterCommit;
-
 
 function output(cm, dontExit) {
     if (!cm) {
@@ -60,70 +51,12 @@ function output(cm, dontExit) {
     }
 }
 
-Git
-    .Repository
-    .open(pwd)
-    .then(repository => {
-        repo = repository;
-        return repo
-            .getMasterCommit()
-            .then(commit => {
-                masterCommit = commit;
-                return program.args.length === 0 ? repo.getHeadCommit() : repo.getReferenceCommit(program.args[0]);
-            });
-    })
-    .then(headCommit => {
-        if (program.zed) {
-            output(message2cm(headCommit.message()));
-        } else if (_.has(program, 'log')) {
-            let history = headCommit.history(),
-                c = program.log,
-                log = [];
+exec(`git log master..HEAD  --pretty=format:\'${LOG_SEPARATOR}%B\'`, (error, stdout) => {
+    let cms = _.map(_.drop(stdout.split(LOG_SEPARATOR)), message2cm);
 
-            history
-                .on('commit', commit => {
-                    if (--c >= 0) {
-                        let obj = message2cm(commit.message());
-
-                        if (obj) {
-                            log.push(obj);
-                        }
-                    }
-                });
-
-            history
-                .on('end', () => {
-                    log.forEach(cm => {
-                        output(cm, true);
-                        console.log('\n\n');
-                    });
-                });
-
-            history.start();
-        } else {
-            let history = headCommit.history(),
-                cm,
-                stop = false;
-
-            history
-                .on('commit', commit => {
-                    if (!stop) {
-                        stop = !program.all && Git.Graph.descendantOf(repo, masterCommit, commit);
-
-                        if (!cm && !stop) {
-                            cm = message2cm(commit.message());
-                        }
-                    }
-                });
-
-            history
-                .on('end', () => {
-                    output(cm);
-                });
-
-            history.start();
-        }
-    })
-    .catch(err => {
-        console.error(err);
-    });
+    if (program.tip) {
+        output(cms.length === 0 ? null : cms[0]);
+    } else {
+        output(_.find(cms, cm => cm));
+    }
+});
