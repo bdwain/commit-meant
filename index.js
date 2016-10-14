bb4b99c10da60fd464a21cac094d7a4be599a80a
Bryan Wain <bryandwain@gmail.com>
2016-10-14 14:14:44 -0500

    Simplify regex

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const log = {},
     exec = require('child_process').exec,
     program = require('commander'),
     NO_COMMIT_MEANT = 'No commit-meant found.',
-    messageRe = /^(MAJOR|MINOR|PATCH) - ([A-Z].+)\n\n((?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?)\n\n((?:[^\*+-].+(?:\n\n)*)*)((?:[\*]\s.+\n*)*)/g,
+    messageRe = /^(MAJOR|MINOR|PATCH).*/g,
     noteRe = /[\*+-]\s/g,
     LOG_SEPARATOR = 'GJVX47gWz4@7m&*uYX%5qe24';
 
